bcdab4a68bfdf4c9c963c7b2519136dfb32cec43
github username <github email address>
2019-12-12 17:28:21 -0600

    authenticate backend done

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -14,8 +14,19 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        user = User.create(user_params)
-        render json: user
+        user = User.new(user_params)
+        if user.valid?
+            user.save
+            render json: {user: UserSerializer.new(user)}, status: :created
+        else
+            render json: {error: 'Failed to create user.'}, status: :not_acceptable
+        end
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user, except: [:created_at, :updated_at]
     end
 
     private
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
+    before_action :check_authentication
+
+    #this creates a token
+    def encode_token(payload)# for token generation
+        JWT.encode(payload, "RentalSecretKey", "HS256") #algo is optioinal as a default HS256
+    end
+
+    def auth_header
+        # { 'Authorization': 'Bearer <token>' }
+        request.headers['Authorization']
+    end
+    
+    #if decoded_token is true decode the token using the user_id and find that user
+    def current_user
+        if decoded_token
+          user_id = decoded_token["user_id"]
+  
+          User.find(user_id)
+        end
+    end
+    
+    def decoded_token
+        if auth_header
+          token = auth_header.split(' ')[1]
+            puts token
+          begin
+           JWT.decode(token,"ProjManageSecretKey")[0] 
+          rescue JWT::DecodeError
+            nil
+          end
+        end
+    end
+    
+    #this is the first function that runs because of before action
+    def check_authentication
+        render json: { error: 'Please log in' }, status: 401 if !logged_in?
+    end
+    
+    #if logged_in is true call a another method current_user
+    def logged_in?
+        #the !! is just finding a vaule true or false
+        !!current_user
+    end
 end
