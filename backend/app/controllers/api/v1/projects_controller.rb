8737a0db651662131562e8f6fe68dc1ba7406779
github username <github email address>
2019-12-17 10:21:13 -0600

    full CRUD for project is done now testing to make sure they all work

--- backend/app/controllers/api/v1/projects_controller.rb
+++ backend/app/controllers/api/v1/projects_controller.rb
@@ -3,5 +3,34 @@ class Api::V1::ProjectsController < ApplicationController
         projects = Project.all
         render json: projects 
     end
-    
+
+    def show 
+        project = Project.find_by(id: params(:id))
+        if project
+            render json: project 
+        else
+            render json: {message: "No project found by that ID"}
+        end
+    end
+
+    def create
+        project = Project.create(project_params)
+        render json: project
+    end
+
+    def update
+        project = Project.find(params[:id])
+        project.update(project_params)
+        render json: project
+    end
+
+    def destroy
+        project = Project.find(params[:id])
+        project.destroy
+    end
+
+    private
+    def project_params
+        params.require(:project).permit(:user_id, :title, :desc, :status)
+    end
 end
