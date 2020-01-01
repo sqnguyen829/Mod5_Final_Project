2157104d81d4757f0c3fb3eb5de4b7166863a5da
github username <github email address>
2020-01-01 12:41:27 -0600

    background added but it need to be static as the user scrolls down

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,26 @@ import Signup from './components/Signup'
 import DefaultPage from './components/DefaultPage'
 import  Home from './containers/Home'
 
-//when using connect App need props arg
+const myBackgroundImg = require('./images/background_bluecode.jpg');
+const divStyle = {
+  width: '100%',
+  height: '800px',
+  backgroundImage: `url(${myBackgroundImg})`,
+  backgroundSize: 'cover'   
+};
+
 function App(props) {
     return (
-      <BrowserRouter>
-          <Switch>
-              <Route exact path = '/' component={DefaultPage}/>
-              <Route path= '/login' component={Login}/>
-              <Route path= '/signup' component={Signup} history={props.history}/>
-              <Route path= '/home' component={Home} history={props.history}/>
-          </Switch>
-      </BrowserRouter>
+        <div className="cComponent" style={divStyle} >
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path = '/' component={DefaultPage}/>
+                    <Route path= '/login' component={Login}/>
+                    <Route path= '/signup' component={Signup} history={props.history}/>
+                    <Route path= '/home' component={Home} history={props.history}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 export default App
