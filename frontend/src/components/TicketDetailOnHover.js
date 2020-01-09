74b9ff158ac12c66bc9d920360212fb7b1dd6ce7
github username <github email address>
2020-01-09 10:25:32 -0600

    Graphs are properly displaying on the home page by row and column

---
+++ frontend/src/components/TicketDetailOnHover.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+function TicketDetailOnHover(props) {
+    // const dispatch = useDispatch()
+    return(
+        <div class="ui button" data-tooltip="Add users to your feed" data-position="top center">
+            Top Center
+        </div>
+    )
+}
+
+export default TicketDetailOnHover
